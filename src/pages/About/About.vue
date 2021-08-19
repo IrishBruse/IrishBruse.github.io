07997ef2bf8a128cd26d2fcce5f7611acd48f78b
Ethan Conneely <Econn50@outlook.com>
2021-08-19 14:22:21 +0100

    Add mobile support to about page

--- src/pages/About/About.vue
+++ src/pages/About/About.vue
@@ -1,14 +1,11 @@
 <template>
     <h1 class="text-center">About Me</h1>
-    <p class="aboutMe text-justify">
+    <p class="aboutMe text-center">
         Hi I'm Ethan an {{ age }} year old studying
         <a href="https://www.gmit.ie/computer-science-and-applied-physics/bachelor-science-honours-software-development">Software Development</a> in GMIT. I am
-        self taught in programming before GMIT and have been programming for many years as a hobby making games, websites, software and mobile apps.
-    </p>
-
-    <p class="aboutMe text-justify">
-        I love Open source software and try to contribute to it where I can. Currently I maintain a library for importing and rendering levels made with an open
-        source game level editor it is hosted on my <a href="https://github.com/IrishBruse">Github profile</a>.
+        self taught in programming before GMIT and have been programming for many years as a hobby making games, websites, software and mobile apps. I love Open
+        source software and try to contribute to it where I can. Currently I maintain a library for importing and rendering levels made with an open source game
+        level editor it is hosted on my <a href="https://github.com/IrishBruse">Github profile</a>.
     </p>
 
     <Skills />
@@ -25,8 +22,14 @@ const age = Math.floor(difference / 31557600000);
 
 <style>
 .aboutMe {
-    width: 60%;
-    margin: 0.5rem auto;
+    width: 100%;
+    margin: 1rem auto;
     font-size: 1.25rem;
+}
+
+@media only screen and (min-width: 992px) {
+    .aboutMe {
+        width: 60%;
+    }
 }
 </style>
