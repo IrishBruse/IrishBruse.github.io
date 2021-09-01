c2397d5b00d33f1e23af6ce0bc1b9481eda7fa9f
Ethan Conneely <Econn50@outlook.com>
2021-09-01 17:00:51 +0100

    Added month word instead of number

--- src/components/projects/ProjectHeader.vue
+++ src/components/projects/ProjectHeader.vue
@@ -19,5 +19,12 @@ const props = defineProps({
     project: Object,
 });
 
-const localDate = new Date(props.project.date).toLocaleDateString();
+var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+function convertDate(date_str) {
+    var temp_date = date_str.split("/");
+    return temp_date[0] + " " + months[Number(temp_date[1]) - 1] + " " + temp_date[2];
+}
+
+const localDate = convertDate(new Date(props.project.date).toLocaleDateString());
 </script>
