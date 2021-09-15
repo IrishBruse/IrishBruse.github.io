0653da31058179c9770238f733cf30799280e61d
Ethan Conneely <Econn50@outlook.com>
2021-09-15 14:59:00 +0100

    Reset deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,3 @@
-git clone git@github.com:IrishBruse/IrishBruse.github.io.git ./dist
-
 npm run build
 # navigate into the build output directory
 cd dist
@@ -8,6 +6,7 @@ cp index.html 404.html
 
 echo 'www.ethanconneely.com' > CNAME
 
+git init
 git add -A
-git commit -m 'deploy: '
+git commit -m 'deploy:'
 git push -f git@github.com:IrishBruse/IrishBruse.github.io.git master:pages
