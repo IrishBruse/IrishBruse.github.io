8b2af6969b4cb326312e8c128e613d1c5d7dfac1
Ethan Conneely <Econn50@outlook.com>
2021-09-21 17:25:16 +0100

    Change over to use Github Releases

--- src/projects/IrishBruse Launcher/Index.vue
+++ src/projects/IrishBruse Launcher/Index.vue
@@ -1,19 +1,16 @@
 <template>
     <ProjectHeader :project="props.project">
-        IrishBruse Launcher is a way to share all my projects and software along with all the versions letting you see the progress in each project.
-        It uses the dropbox Api to download the projects from their cloud service and then unzips the game ready to play. Its all gotten live from
-        the service so i can add and remove games, software and versions as I want.
-        Its the first golang application I wrote too as i wanted to give a try at a new language and go had some handy features that made making
-        this application alot easier than in C#.
+        IrishBruse Launcher is a way to share all my projects and software along with all the versions letting you see the progress in each project. It uses the
+        dropbox Api to download the projects from their cloud service and then unzips the game ready to play. Its all gotten live from the service so i can add
+        and remove games, software and versions as I want. Its the first golang application I wrote too as i wanted to give a try at a new language and go had
+        some handy features that made making this application alot easier than in C#.
     </ProjectHeader>
 
     <div class="download">
-        <button class="round centerButton" @click="downloadInstaller">
-            Download
-        </button>
+        <button class="round centerButton" @click="downloadInstaller">Download</button>
     </div>
 
-    <iframe ref="downloader" src="" style="display:none;" />
+    <iframe ref="downloader" src="" style="display: none" />
 </template>
 
 <script setup>
@@ -26,19 +23,18 @@ const props = defineProps({
 
 const downloader = ref();
 
-const downloadInstaller = ()=>{
-    downloader.value.src = "/InstallerV2.exe";
-}
+const downloadInstaller = () => {
+    downloader.value.src = "https://github.com/IrishBruse/Launcher/releases/download/v1.1.0/Installer.exe";
+};
 </script>
 
 <style>
-
-.download{
+.download {
     display: flex;
     justify-content: center;
 }
 
-.centerButton{
+.centerButton {
     margin: 2rem 0;
     align-items: center;
     width: 10rem;
@@ -47,7 +43,7 @@ const downloadInstaller = ()=>{
     font-weight: bolder;
     background-color: var(--link);
 }
-.centerButton:hover{
+.centerButton:hover {
     background-color: var(--link-focus);
 }
 </style>
